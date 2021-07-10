c2d0d4073571ef041fb2e99972292d928b284ecf
julianacnascimento <juliana.conceicao@dce.ufpb.br>
2021-07-10 12:31:29 -0300

    versao atual

---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+import { api } from "../../services/api"
+import { Container } from "./styles"
+
+export function TransactionTable(){
+
+  useEffect(()=>{
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th> Título </th>
+            <th> Valor </th>
+            <th> Categoria </th>
+            <th> Data </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td> Desenvolvimento de website </td>
+            <td className="deposit"> R$12.000 </td>
+            <td> Desenvolvimento </td>
+            <td> 04/07/21 </td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td> Aluguel </td>
+            <td className="withdraw">- R$1.100 </td>
+            <td> Casa </td>
+            <td> 23/06/21 </td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
